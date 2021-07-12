565a5d8d1eff69ed27f421e8ad3fe7ee40daea8b
Mark Florence <mflo999@gmail.com>
2021-07-12 10:39:32 -0400

    Prepare new version for release

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,14 @@
-version=${1:patch}
+#!/bin/bash 
+
+version=${1:-patch}
+message=${2:-"Prepare new version for release"}
+
+echo $version
 
 git add . *
-git commit -m "Prepare new version for release"
+git commit -m "$message"
 git push origin main
 
-npm version ${version}
+npm version $version
 
 cp -R www/* ../mflorence99.github.io
