467b492010bba82baec7e0c3ff3d8413df18fb43
Mark Florence <mflo999@gmail.com>
2021-08-25 01:55:48 -0400

    Prepare new version for release

---
+++ bin/deploy-aws.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+now=$(date)
+echo "{ \"date\": \"$now\", \"id\": $RANDOM }" > src/assets/build.json
+
+version=${1:-patch}
+message=${2:-"Prepare new version for release"}
+
+git add . *
+git commit -m "$message"
+
+npm version $version
+
+git push origin main
+
+ionic build --prod
+
+serverx-angular --app www --deploy aws.json
